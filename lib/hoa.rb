b7e5b239e78cd729bb00499b53de3bbed7205f79
Daniel Succar <danielsuccar@icloud.com>
2020-01-22 23:55:58 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -10,7 +10,7 @@ def add_character(show, name)
 add_character = {
   :chipmunks => {
      Dave: ["Alvin", "Simon", "Theodore"]
-   }
+   },
   :third_earthers => ["Lion-O", "Cheetara", "Mumm-Ra (the ever-living)"],
   :jetsons => ["George", "Jane", "Judy", "Elroy"]
 }
