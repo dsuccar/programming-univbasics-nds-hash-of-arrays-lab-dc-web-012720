f1ee366ef3bcc9643904b83d2122e33a7d847653
Daniel Succar <danielsuccar@icloud.com>
2020-01-22 23:51:37 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -8,8 +8,8 @@ BASE_HOA = {
 def add_character(show, name)
 
 add_character {
-  :chipmunks => ["Alvin", "Simon", "Theodore"],
-  :third_earthers => ["Lion-O", "Cheetara", "Mumm-Ra (the ever-living)"],
+  :chipmunks => ["Alvin", "Simon", "Theodore"]
+  :third_earthers => ["Lion-O", "Cheetara", "Mumm-Ra (the ever-living)"]
   :jetsons => ["George", "Jane", "Judy", "Elroy"]
 }
   # Write your implementation here
