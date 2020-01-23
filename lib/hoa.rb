6e053103f8d8908120b382f0532b94a1f7dc75c2
Daniel Succar <danielsuccar@icloud.com>
2020-01-23 00:00:27 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -1,4 +1,4 @@
-BASE_HOA = {
+add_character = {
   :chipmunks => ["Alvin", "Simon", "Theodore"],
   :third_earthers => ["Lion-O", "Cheetara", "Mumm-Ra (the ever-living)"],
   :jetsons => ["George", "Jane", "Judy", "Elroy"]
